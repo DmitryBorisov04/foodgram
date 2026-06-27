eb3018c10e8ec468f29007532af73bfc162fcf2e
Дмитрий Борисов <exizto13@gmail.com>
2026-06-27 15:07:31 +0300

    Admin zone recipes is done

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Recipe, RecipeIngredient, RecipeTag, Ingredients, Tags
 
-# Register your models here.
+
+@admin.register(Ingredients)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'unit')
+    search_fields = ('name',)
+
+
+@admin.register(Tags)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+
+class RecipeTagInline(admin.TabularInline):
+    model = RecipeTag
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'created_at')
+    list_filter = ('tags',)
+    search_fields = ('name', 'author__username')
+    inlines = [RecipeIngredientInline, RecipeTagInline]
